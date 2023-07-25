5a5610f92b256cbabeba70a203fd47bfc9fd041b
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-07-24 22:33:09 -0300

    feat: created initial routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,43 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Login } from "../pages/login";
+import { Home } from "../pages/Home";
+import { Settings } from "../pages/Settings";
 
 export function Routes() {
   const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   const Authenticate = false;
 
+  function App() {
+    return (
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    );
+  }
+
   function LoginScreen() {
     return (
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="App">
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="App"
+          component={App}
           options={{
             headerShown: false,
           }}
