fa35eaffedd4c8eb1a1fe94919c068870d7bc84d
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-07-24 22:33:36 -0300

    feat: added new fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "./src/routes/routes";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <NavigationContainer>
-      <StatusBar translucent style="light" />
+      <StatusBar translucent style="dark" />
       <Routes />
     </NavigationContainer>
   );
