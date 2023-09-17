cf287edeadbf053f44f4f30ad06972792a2748bf
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-09-17 17:44:19 -0300

    feat: inicialized transactions screen

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export function Transactions() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ height: 50, backgroundColor: "black" }}></View>
+    </SafeAreaView>
+  );
+}
