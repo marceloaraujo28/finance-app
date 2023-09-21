d7417b0f1d801bf88cb53fff31ab7bf2b74c33e2
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-09-21 01:02:14 -0300

    feat: updated color header

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -5,7 +5,7 @@ import { Header } from "./components/Header";
 
 export function Transactions() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#2754cf" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#1749d1" }}>
       <Header />
       <RouteTabs />
     </SafeAreaView>
