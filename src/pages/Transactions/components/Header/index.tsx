4fce09e244c938ee2287b347a8ff0b9e171a1b73
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-09-21 01:02:49 -0300

    feat: stylized header

--- src/pages/Transactions/components/Header/index.tsx
+++ src/pages/Transactions/components/Header/index.tsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import * as S from "./styles";
+import { MonthPicker } from "../../../../components/MonthPicker";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export function Header() {
+  const [date, setDate] = useState(new Date());
+
   return (
-    <View
-      style={{
-        height: 50,
-        backgroundColor: "#2754cf",
-      }}
-    >
-      <Text
-        style={{
-          color: "white",
-        }}
-      >
-        Calendário e Gráfico
-      </Text>
-    </View>
+    <S.Container>
+      <S.DatePicker>
+        <MonthPicker date={date} />
+      </S.DatePicker>
+      <S.Chart>
+        <MaterialIcons name="insert-chart-outlined" size={24} color="white" />
+      </S.Chart>
+    </S.Container>
   );
 }
